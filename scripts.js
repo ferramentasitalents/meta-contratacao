b8ce5a87dd3bd0abdbdf1db4979a0818c8cd1949
ferramentasitalents <133061937+ferramentasitalents@users.noreply.github.com>
2023-05-16 11:16:19 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
+function calculateMeta() {
+  const numContratacoes = parseFloat(document.getElementById("num-contratacoes").value);
+  const metaContratacoes = parseFloat(document.getElementById("meta-contratacoes").value);
+  const metaPercent = (numContratacoes / metaContratacoes) * 100;
+  document.getElementById("result").innerHTML = `Meta de Contratação: ${metaPercent.toFixed(2)}%`;
+}
 
